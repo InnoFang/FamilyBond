2f86dc0b075959e00d693fb84a2a2f4752a99f03
InnoFang <q984941707@qq.com>
2017-09-09 17:50:54 +0800

    [UPDATE] test for AMap

--- test/src/main/java/io/innofang/test/MainActivity.java
+++ test/src/main/java/io/innofang/test/MainActivity.java
@@ -1,7 +1,9 @@
 package io.innofang.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.amap_button:
+                startActivity(new Intent(this, AMapActivity.class));
+                break;
+        }
     }
 }
