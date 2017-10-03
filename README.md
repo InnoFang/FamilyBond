8b06e3eb6ddfbde80cda9e691202779fa7e2beaf
InnoFang <q984941707@qq.com>
2017-10-03 15:28:34 +0800

    [update] clean.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ProtectPlus
+# FamilyBond
 
 An application, combine with Children side port and Parents side port, which let children take good care of their parents,
 as well as parents who are older than 65 or more can easily use smart phone.
--- parents/src/main/java/io/innofang/parents/MainActivity.java
+++ parents/src/main/java/io/innofang/parents/MainActivity.java
@@ -37,9 +37,9 @@ import io.innofang.base.bean.User;
 import io.innofang.base.util.bmob.BmobEvent;
 import io.innofang.base.util.bmob.BmobUtil;
 import io.innofang.base.util.common.BottomNavigationViewHelper;
+import io.innofang.medically.heat_beat.HeartBeatOldFragment;
 import io.innofang.parents.communication.CommunicationFragment;
 import io.innofang.parents.home.HomeFragment;
-import io.innofang.parents.medically_exam.MedicallyExamFragment;
 import io.innofang.parents.settings.SettingsFragment;
 import io.innofang.parents.sms.SmsFragment;
 
@@ -103,7 +103,7 @@ public class MainActivity extends BaseActivity
         });
         List<Fragment> list = new ArrayList<>();
         list.add(SmsFragment.newInstance());
-        list.add(MedicallyExamFragment.newInstance());
+        list.add(HeartBeatOldFragment.newInstance());
         list.add(HomeFragment.newInstance());
         list.add(CommunicationFragment.newInstance());
         list.add(SettingsFragment.newInstance());
