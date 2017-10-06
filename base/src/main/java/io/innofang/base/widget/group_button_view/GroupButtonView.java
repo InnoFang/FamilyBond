3e9aeb89ef110d3e309fc42666b5cbe215069831
InnoFang <q984941707@qq.com>
2017-10-06 11:45:44 +0800

    [update] add new widget

---
+++ base/src/main/java/io/innofang/base/widget/group_button_view/GroupButtonView.java
@@ -0,0 +1,123 @@
+package io.innofang.base.widget.group_button_view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import io.innofang.base.R;
+
+/**
+ * Author: Inno Fang
+ * Time: 2017/10/6 11:35
+ * Description:
+ */
+
+public class GroupButtonView extends LinearLayout {
+
+    RadioGroup mRg;
+
+    private Context mContext;
+    private OnGroupBtnClickListener listener;
+
+    private String groupBtnName = "A#B";
+    private String groupBtnCode = "1#2";
+
+    public GroupButtonView(Context context) {
+        this(context, null);
+    }
+
+    public GroupButtonView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public GroupButtonView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.mContext = context;
+
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.GroupButtonView);
+        groupBtnName = ta.getString(R.styleable.GroupButtonView_groupBtnNameText);
+        groupBtnCode = ta.getString(R.styleable.GroupButtonView_groupBtnCodeText);
+
+        ta.recycle();
+
+        initContentView();
+        initView();
+        initData();
+    }
+
+    private void initContentView() {
+        View.inflate(mContext, R.layout.view_item_group_buttons, this);
+
+        mRg = (RadioGroup) findViewById(R.id.rg_item_group_btn);
+
+    }
+
+    private void initView() {
+        String[] btnNameArr = groupBtnName.split("#");
+        String[] btnCodeArr = groupBtnCode.split("#");
+
+        Log.d("gyw", btnNameArr.length + "   length  :" + btnCodeArr.length);
+
+        if ((btnNameArr.length < 2) || (btnCodeArr.length < 2)) {
+            throw new RuntimeException("names和codes的格式不正确...");
+        }
+
+        for (int i = 0; i < btnCodeArr.length; i++) {
+
+            //使用布局文件写方便
+            RadioButton rb = (RadioButton) View.inflate(mContext, R.layout.view_item_group_radio_button, null);
+
+            if (btnCodeArr.length == 2) {
+                if (i == 0) {
+                    rb.setBackgroundResource(R.drawable.left_button_selector);
+                    rb.setChecked(true);
+                } else if (i == 1) {
+                    rb.setBackgroundResource(R.drawable.right_button_selector);
+                }
+            } else {
+                if (i == 0) {
+                    rb.setBackgroundResource(R.drawable.left_button_selector);
+                    rb.setChecked(true);
+                } else if (i == btnCodeArr.length - 1) {
+                    rb.setBackgroundResource(R.drawable.right_button_selector);
+                } else {
+
+                    rb.setBackgroundResource(R.drawable.mid_button_selector);
+                }
+            }
+
+            rb.setId(i);
+            rb.setTag(btnCodeArr[i]);
+            rb.setText(btnNameArr[i]);
+
+            mRg.addView(rb);
+        }
+    }
+
+    private void initData() {
+        mRg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup rg, int i) {
+                RadioButton rb = (RadioButton) findViewById(i);
+                if (rb.isChecked()) {
+                    if (listener != null) {
+                        listener.groupBtnClick(rb.getTag().toString());
+                    }
+                }
+            }
+        });
+    }
+
+    public interface OnGroupBtnClickListener {
+        public void groupBtnClick(String code);
+    }
+
+    public void setOnGroupBtnClickListener(OnGroupBtnClickListener listener) {
+        this.listener = listener;
+    }
+}
